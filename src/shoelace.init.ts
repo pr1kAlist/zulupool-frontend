b33a9717d3d8165dc8c9ed34790e12393d3b250f
pr1kAlist <pr1k@list.ru>
2020-10-06 20:41:05 +0300

    dev

--- src/shoelace.init.ts
+++
@@ -1,8 +0,0 @@
-import {
-    setAssetPath,
-    SlButton,
-} from "@shoelace-style/shoelace/dist/custom-elements";
-
-setAssetPath("/assets/icons-shoelace/");
-
-customElements.define("sl-button", SlButton);
